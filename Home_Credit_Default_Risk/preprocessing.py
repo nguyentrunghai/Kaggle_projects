341a1dba56f138a9c02c409ed9f6d1b9529fe079
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-12-09 15:33:03 +0700

    add function print_long_string()

--- Home_Credit_Default_Risk/preprocessing.py
+++ Home_Credit_Default_Risk/preprocessing.py
@@ -5,3 +5,19 @@ Define functions for data preprocessing
 import pandas as pd
 import numpy as np
 
+
+def print_long_string(string, indent=0, max_words_per_line=10):
+    """
+    :param string: str
+    :param indent: int
+    :param max_words_per_line: int
+    :return: None
+    """
+    words = [" "*indent]
+    for i, word in enumerate(string.split()):
+        words.append(word)
+        if (i+1) % max_words_per_line == 0:
+            words.append("\n" + " "*indent)
+    print(" ".join(words))
+    return None
+
