124fb5fc9594e326e31d62741014a349f2153c46
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-12-12 13:55:25 +0700

    remove key-value from params_fixed if it is in params_grid or params_dist

--- Home_Credit_Default_Risk/_model_tunning.py
+++ Home_Credit_Default_Risk/_model_tunning.py
@@ -6,24 +6,27 @@ from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 
 def grid_search(X_train, y_train, estimator_constructor,
-                params_fixed, param_grid, scoring, cv):
+                params_fixed, params_grid, scoring, cv):
     """
     :param X_train: dataframe or array, training input features
     :param y_train: arryay, training labels
     :param estimator_constructor: name of the class from which estimator is constructed
     :param params_fixed: dict, hyperparameters which are kept fixed. They are passed to the constructor
-    :param param_grid: dict, value grid of hyperparameters which are tuned. They are passed to GridSearchCV
+    :param params_grid: dict, value grid of hyperparameters which are tuned. They are passed to GridSearchCV
     :param scoring: str, scoring metric
     :param cv: int, or an instance of StratifiedKFold
     :return: best_estimator, best_params, best_score
     """
+    for param in params_grid:
+        params_fixed.pop(param, None)
+
     estimator = estimator_constructor(**params_fixed)
-    gs = GridSearchCV(estimator=estimator, param_grid=param_grid, scoring=scoring,  cv=cv)
+    gs = GridSearchCV(estimator=estimator, param_grid=params_grid, scoring=scoring,  cv=cv)
     gs.fit(X_train, y_train)
 
     best_params = gs.best_params_
     print("Best parameters:", best_params)
-    
+
     best_estimator = gs.best_estimator_
     # fit best_estimator on whole training dataset
     best_estimator.fit(X_train, y_train)
@@ -37,7 +40,7 @@ def grid_search(X_train, y_train, estimator_constructor,
 
 
 def randomized_search(X_train, y_train, estimator_constructor,
-                      params_fixed, param_dist, n_iter,
+                      params_fixed, params_dist, n_iter,
                       scoring, cv,
                       random_state=None):
     """
@@ -45,7 +48,7 @@ def randomized_search(X_train, y_train, estimator_constructor,
     :param y_train: arryay, training labels
     :param estimator_constructor: name of the class from which estimator is constructed
     :param params_fixed: dict, hyperparameters which are kept fixed. They are passed to the constructor
-    :param param_dist: dict, key are hyperparameter names, values are list of float or a scipy distribution.
+    :param params_dist: dict, key are hyperparameter names, values are list of float or a scipy distribution.
                         They are passed to RandomizedSearchCV.
     :param n_iter: int
     :param scoring: str, scoring metric
@@ -53,9 +56,12 @@ def randomized_search(X_train, y_train, estimator_constructor,
     :param random_state: int or RandomState instance or None
     :return: best_estimator, best_params, best_score
     """
+    for param in params_dist:
+        params_fixed.pop(param, None)
+
     estimator = estimator_constructor(**params_fixed)
 
-    rs = RandomizedSearchCV(estimator=estimator, param_distributions=param_dist,
+    rs = RandomizedSearchCV(estimator=estimator, param_distributions=params_dist,
                             n_iter=n_iter, scoring=scoring, refit=False, cv=cv,
                             random_state=random_state)
     rs.fit(X_train, y_train)
