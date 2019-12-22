50691ca34fe53ca9c4cd68652e0d4eeb50193939
Trung Haii Nguyen <nguyentrungh@gmail.com>
2019-12-22 15:29:47 +0700

    add function corrwith

--- Home_Credit_Default_Risk/_stats.py
+++ Home_Credit_Default_Risk/_stats.py
@@ -21,3 +21,23 @@ def within_group_variance(group_variances, weights):
 
 def f_ratio(group_averages, group_variances, weights):
     return between_group_variance(group_averages, weights) / within_group_variance(group_variances, weights)
+
+
+def corrwith(df, series):
+    """
+    :param df: a dataframe
+    :param series: a series
+    :return: corrs, a series
+    """
+    corrs = df.corrwith(series)
+    corrs = corrs.reset_index()
+    corrs.columns = ["feature", "corr"]
+
+    corrs["abs_corr"] = np.abs(corrs["corr"])
+    corrs = corrs.sort_values("abs_corr", ascending=False)
+    corrs = corrs.set_index("feature")
+
+    corrs = corrs.drop(["abs_corr"], axis=1)
+
+    return corrs["corr"]
+
